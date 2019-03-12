8365fab536ca1efea2331a32b21faf29a88a21fd
xuxin199601 <34016726+xuxin199601@users.noreply.github.com>
2019-03-12 10:48:48 +0800

    增加错误码，统一异常处理

--- src/main/java/com/csu/lab/enums/ResultEnum.java
+++ src/main/java/com/csu/lab/enums/ResultEnum.java
@@ -1,10 +1,24 @@
 package com.csu.lab.enums;
 
 public enum ResultEnum {
-    UNKONW_ERROR(-1, "未知错误"),
+    UNKONW_ERROR(-1, "系统异常"),
+
     SUCCESS(0, "成功"),
-    NO_FOUND(101, "未找到信息"),
+
     ACCOUNT_OR_PASSWORD_ERROR(1,"账号或密码错误"),
+
+    // 保存导师信息时的错误
+    TUTOR_EXIST(1, "Tutor Exist."),
+    SAVE_FAILURE(2, "Failure."),
+
+    // 修改导师信息时的错误
+    UPDATE_FAILURE(1, "Failure."),
+
+    // 查询导师信息时的错误
+    TUTOR_NO_FOUND(1, "Tutor No Found."),
+
+    // 删除导师信息时的错误
+    DELETE_FAILURE(1, "Failure.")
 
     ;
 
