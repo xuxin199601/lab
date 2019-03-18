bc6cbdc8253e6bdd318afb088c8703d6a8c64dfb
zhangqi <1795638289@qq.com>
2019-03-18 10:24:23 +0800

    修改获取研究方向列表方法

--- src/main/java/com/csu/lab/controller/DirectionController.java
+++ src/main/java/com/csu/lab/controller/DirectionController.java
@@ -3,12 +3,16 @@ package com.csu.lab.controller;
 import com.csu.lab.pojo.Direction;
 import com.csu.lab.pojo.Message;
 import com.csu.lab.service.DirectionService;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("/server/direction")
 public class DirectionController {
 
@@ -17,10 +21,26 @@ public class DirectionController {
 
     // 分页研究方向信息列表
     @GetMapping("/directionList")
-    public Message getStudentByPage(@RequestParam("page_index")Integer pageIndex,
-                                    @RequestParam("page_size")Integer pageSize) {
-        List<Direction> directionList = directionService.queryDirectionListPaged(pageIndex, pageSize);
-        return Message.success().add(directionList);
+    public String getStudentByPage(@RequestParam(defaultValue = "1") Integer pageNum,
+                                    @RequestParam(defaultValue = "10") Integer pageSize,
+                                    Model model) {
+        PageHelper.startPage(pageNum,pageSize);
+
+        List<Direction> directionList = directionService.getDirectionList();
+        PageInfo pageInfo = new PageInfo(directionList,10);
+
+        model.addAttribute("pageInfo",pageInfo);
+        //获得当前页
+        model.addAttribute("pageNum",pageInfo.getPageNum());
+        //获得一页显示的条数
+        model.addAttribute("pageSize",pageInfo.getPageSize());
+        //是否是第一页
+        model.addAttribute("isFirstPage",pageInfo.isIsFirstPage());
+        //获得总页数
+        model.addAttribute("totalPages",pageInfo.getPages());
+        //是否是最后一页
+        model.addAttribute("isLastPage",pageInfo.isIsLastPage());
+        return "server/direction/directionManage";
     }
 
     // 通过id获取研究方向信息
