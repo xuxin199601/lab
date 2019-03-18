8cf27cbfabdbb6b4adaddf47d2345112105ac7d5
zhangqi <1795638289@qq.com>
2019-03-18 17:18:07 +0800

    添加修改实验室信息界面

--- src/main/java/com/csu/lab/controller/LaboratoryController.java
+++ src/main/java/com/csu/lab/controller/LaboratoryController.java
@@ -5,10 +5,8 @@ import com.csu.lab.service.LaboratoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -21,7 +19,7 @@ public class LaboratoryController {
 
     // 实验室信息展示，只有单条信息，只获取第一条数据
     @RequestMapping("/laboratoryList")
-    public String getStudentByPage(Model model) {
+    public String getStudentByPage(Model model, @RequestParam("error") String error) {
         List<Laboratory> list = laboratoryService.getLaboratoryList();
 
         model.addAttribute("laboratory", list.get(0));
@@ -30,16 +28,16 @@ public class LaboratoryController {
 
     // 修改实验室信息
     @PostMapping("/modifyLaboratory")
-    public String modifyLaboratory(Laboratory laboratory, Model model) {
+    public String modifyLaboratory(Laboratory laboratory, RedirectAttributes attr) {
 
         System.out.println(laboratory.getLid());
 
         int result = laboratoryService.updateLaboratory(laboratory);
 
         if (result == 1) {
-            model.addAttribute("error", "修改成功");
+            attr.addAttribute("error", "修改成功");
         } else {
-            model.addAttribute("error", "修改失败");
+            attr.addAttribute("error", "修改失败");
         }
         return "redirect:laboratoryList";
     }
