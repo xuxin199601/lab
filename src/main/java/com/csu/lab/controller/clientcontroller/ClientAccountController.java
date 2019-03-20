eaa090d78e7e432b0840d82638b1afe65415108b
gulite <455819483@qq.com>
2019-03-20 11:07:17 +0800

    添加两个空的ClientController

--- src/main/java/com/csu/lab/controller/clientcontroller/ClientAccountController.java
+++ src/main/java/com/csu/lab/controller/clientcontroller/ClientAccountController.java
@@ -11,9 +11,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpSession;
 import java.io.UnsupportedEncodingException;
 import java.security.NoSuchAlgorithmException;
 import java.util.Calendar;
@@ -27,8 +29,29 @@ import java.util.List;
 @RequestMapping("/client/account")
 public class ClientAccountController {
 
+    String msg;
+
     @Autowired
     AccountService accountService;
 
+    // 跳转到账户管理界面(该方法有问题，未完成)
+    @RequestMapping("/account")
+    public String accountList(@RequestParam(name = "id", required = false) String aid,
+                              Model model,
+                              HttpSession session) {
+
+        //session.setAttribute("error", msg);
+        msg = null;
+
+        List<Account> list;
+        if (aid != null){
+            model.addAttribute("key", aid);
+            list = accountService.queryByProperty("username", aid);
+        }else {
+            list = accountService.getAccountList();
+        }
+
+        return "client/account";
+    }
 
 }
---
+++ src/main/java/com/csu/lab/controller/clientcontroller/ClientLoginController.java
@@ -0,0 +1,20 @@
+package com.csu.lab.controller.clientcontroller;
+
+
+import com.csu.lab.pojo.Account;
+import com.csu.lab.service.AccountService;
+import com.csu.lab.service.ResearcherService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("/client")
+public class ClientLoginController {
+
+}
