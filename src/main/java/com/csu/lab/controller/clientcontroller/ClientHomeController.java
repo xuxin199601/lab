f4627b83e91d8a7975875dcfdfa6fcd465f12331
gulite <455819483@qq.com>
2019-03-26 22:15:27 +0800

    完成了登入登出

--- src/main/java/com/csu/lab/controller/clientcontroller/ClientHomeController.java
+++ src/main/java/com/csu/lab/controller/clientcontroller/ClientHomeController.java
@@ -1,11 +1,10 @@
 package com.csu.lab.controller.clientcontroller;
 
-import com.csu.lab.pojo.Activity;
-import com.csu.lab.pojo.Laboratory;
-import com.csu.lab.pojo.Message;
+import com.csu.lab.pojo.*;
 import com.csu.lab.service.AccountService;
 import com.csu.lab.service.ActivityService;
 import com.csu.lab.service.LaboratoryService;
+import com.csu.lab.service.ResearcherService;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.github.pagehelper.PageRowBounds;
@@ -15,6 +14,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -27,24 +28,42 @@ public class ClientHomeController {
     @Autowired
     private ActivityService activityService;
 
+    @Autowired
+    private ResearcherService  researcherService;
+
     //跳转到index界面
     @RequestMapping("/index")
-    public String doIndex(Model model){
-       /* List<Laboratory> laboratoryList = laboratoryService.getLaboratoryList();
-        List<Activity> activityList = activityService.getActivityList();
+    public String doIndex(Model model ,HttpSession session){
 
-        //分页处理activity里的信息放到首页（应该做成逆序查询，待改）
-        PageInfo pageInfo = new PageInfo(activityList,5);
-        model.addAttribute("pageInfo",pageInfo);
+        /*System.out.println("session是："+ session.getAttribute("user"));
+        boolean sessionState = false;
+        //如果获取到了session，则把登录成功的状态返回给前端
+        if (session.getAttribute("user")!=null) {
+            sessionState = true;
 
-        model.addAttribute("laboratory" ,laboratoryList.get(0));
-        model.addAttribute("activity" ,activityList.get(0));*/
+            System.out.println("sessionState是：" + sessionState);
+            Account account = (Account) session.getAttribute("user");
+            System.out.println("account是：" + account);
+
+            //通过aid查询用户，获得个人信息
+            List<Researcher> researcherList = researcherService.queryByProperty("aid", account.getAid());
+            model.addAttribute("researcher", researcherList.get(0));
+        }
+        model.addAttribute("sessionState", sessionState);
+        System.out.println("model里有"+model);*/
+
+        boolean sessionState = true;
+        System.out.println(session.getAttribute("user"));
+        if (session.getAttribute("user")!=null)
+            sessionState = false;
+
+        model.addAttribute("sessionState", sessionState);
 
         return "client/index";
     }
 
     @RequestMapping("/home")
-    public String doHome(Model model){
+    public String doHome(Model model ){
         List<Laboratory> laboratoryList = laboratoryService.getLaboratoryList();
         List<Activity> activityList = activityService.getActivityList();
 
--- src/main/java/com/csu/lab/controller/clientcontroller/ClientLoginController.java
+++ src/main/java/com/csu/lab/controller/clientcontroller/ClientLoginController.java
@@ -2,6 +2,7 @@ package com.csu.lab.controller.clientcontroller;
 
 
 import com.csu.lab.pojo.Account;
+import com.csu.lab.pojo.Researcher;
 import com.csu.lab.service.AccountService;
 import com.csu.lab.service.ResearcherService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,9 +13,58 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @Controller
 @RequestMapping("/client")
 public class ClientLoginController {
+
+    @Autowired
+    AccountService accountService;
+
+    @Autowired
+    HttpServletRequest httpServletRequest;
+
+    @Autowired
+    ResearcherService researcherService;
+
+
+    //跳转到登录界面
+    @RequestMapping("/login")
+    public String doLoginLab() {
+        return "client/account/login";
+    }
+
+    //登录实现
+    @RequestMapping(value = "/loginCheck",method = RequestMethod.POST)
+    public String doLogin(Account account, Model model, HttpSession session) {
+        //boolean sessionState = true;
+        Account user = accountService.loginVaildata(account);
+
+        if (user != null) {
+            session.setAttribute("user", user);
+            //sessionState = false;
+
+            //通过aid查询用户，获得个人信息
+            List<Researcher> researcherList = researcherService.queryByProperty("aid",user.getAid());
+            model.addAttribute("researcher",researcherList.get(0));
+            //model.addAttribute("sessionState", sessionState);
+
+            //return "redirect:/client/index";
+            return "client/index";
+        } else {
+            model.addAttribute("error", "用户名或密码错误，请重新登录！");
+            return "client/account/login";
+        }
+    }
+
+    @RequestMapping("/loginOut")
+    public String loginOut(HttpSession session,Model model){
+        boolean sessionState = false;
+        session.setAttribute("user", null);
+        sessionState = false;
+        model.addAttribute("sessionState", sessionState);
+        return "redirect:/client/index";
+    }
 
 }
